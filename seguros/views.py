1f9e72beba169c7e164fb43f5b7dd9e6b6275e94
flarxx <flarx3@gmail.com>
2025-10-14 10:19:50 -0400

    integrado sistema de login para cliente y admin

--- seguros/views.py
+++ seguros/views.py
@@ -1,4 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required, user_passes_test
 
-def index(request):
-    return render(request, 'seguros/layout.html')
+# Vista de inicio general
+def inicio(request):
+    return render(request, 'seguros/base.html')
+
+# Vista inicio para clientes (usuarios normales)
+@login_required
+def inicio_cliente(request):
+    return render(request, 'cliente/inicio_cliente.html')
+
+# Vista inicio para administradores (solo para usuarios staff o superuser)
+@login_required
+def inicio_admin(request):
+    return render(request, 'administrador/inicio_admin.html')
+
+# Vista de login general
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            # Redirigir según tipo de usuario
+            if user.is_staff or user.is_superuser:
+                return redirect('inicio_administrador')  # Redirección a la vista admin
+            else:
+                return redirect('inicio_cliente')        # Redirección a la vista cliente
+        else:
+            messages.error(request, 'Usuario o contraseña incorrectos')
+    return render(request, 'seguros/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def registro(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        User.objects.create_user(username=username, email=email, password=password)
+        messages.success(request, 'Usuario creado correctamente')
+        return redirect('login')
+    return render(request, 'seguros/registro.html')
+
