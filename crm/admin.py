1e8de444de1472b7d6fdaa8b3787f6e6bfe6f307
flarxx <flarx3@gmail.com>
2025-11-19 12:01:28 -0400

    siguiente paso seria hacer todo interactivo con el inicio_cliente.html vamos a ver

--- crm/admin.py
+++ crm/admin.py
@@ -1,3 +1,9 @@
+# crm/admin.py
 from django.contrib import admin
+from .models import Interaccion
 
-# Register your models here.
+@admin.register(Interaccion)
+class InteraccionAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'agente', 'tipo', 'estado', 'poliza', 'fecha_creacion')
+    list_filter = ('tipo', 'estado', 'fecha_creacion')
+    search_fields = ('cliente__username', 'agente__username', 'nota')
